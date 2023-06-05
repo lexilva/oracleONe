7b1ef412971f3b97b784a55c3e467c6dde75550d
Lecnote <lexilva@gmail.com>
2023-06-04 21:28:26 -0300

    Conclusão do desafio

--- desafio/desafio.js
+++ desafio/desafio.js
@@ -2,9 +2,6 @@
 var auxiliar ="";
 var mensagemEncriptada ='';
 var mensagemDecriptada = '';
-function imprimeNaTela(msg){
-	document.write('<h1>'+ msg + '</h1>');
-}
 function ehvogal(letra){
 	if((letra === 'a')||(letra ==='e')||(letra === 'i')||(letra === 'o')||(letra ==='u')){
 			return 1;
@@ -25,26 +22,55 @@ function codificaVogal(vogal){
 		return 'ufat';
 	}
 }
+function filtraMensagem(str){
+	var frase = str;
+	frase = frase.normalize('NFD').replace(/[\u0300-\u036f]/g,"");
+	frase = frase.toLowerCase().replace(/[^a-z]/g,"");
+	return frase;
+}
 function decriptar(){
-	mensagemDecriptada = mensagemEncriptada.replace(/ai/g, 'a');
-	let temp = mensagemDecriptada.replace(/enter/g, 'e');
-	mensagemDecriptada = temp.replace(/imes/g, 'i');
-	temp = mensagemDecriptada.replace(/ober/g, 'o');
+	mensagemDecriptada = mensagemEncriptada.replace(/ai/gi, 'a');
+	let temp = mensagemDecriptada.replace(/enter/gi, 'e');
+	mensagemDecriptada = temp.replace(/imes/gi, 'i');
+	temp = mensagemDecriptada.replace(/ober/gi, 'o');
 	mensagemDecriptada = temp.replace(/ufat/g, 'u');
-	return document.write(mensagemDecriptada);
+	let div = document.querySelector('#botaoCopiar');
+	resolucao.removeChild(div);
+	let resposta = document.querySelector('#resposta');
+	resposta.value = mensagemDecriptada;
 }
 function encriptar(){
 	let msg = textoDigitado.value;
+	msg = filtraMensagem(msg);
+	let resposta = document.querySelector('#resposta');
 	for (var i = 0; i < msg.length;i++){
 			if(ehvogal(msg.charAt(i)) ){
 				mensagemEncriptada+= codificaVogal(msg[i]);	
 			}else{
 				mensagemEncriptada += msg[i]
 			}	
-		}	
+	}	
+	textoDigitado.value = "";
+	resolucao.removeChild(img);
+	resolucao.removeChild(p);
+	resposta.textContent = mensagemEncriptada;
 }
-let textoDigitado = document.getElementById('textoDeEntrada');
-let btncriptografar = document.getElementById('btncodifica');
+var textoDigitado = document.getElementById('input');
+var btncriptografar = document.querySelector('#btncodifica');
 let btndescriptografar = document.getElementById('btndescodifica');
+let resolucao = document.querySelector('#espacoResposta');
+let img = document.querySelector('#pesquisa');
+let p = document.querySelector('section p');
 btncriptografar.onclick = encriptar;
 btndescriptografar.onclick = decriptar;
+let btnCopiar = document.querySelector('#btncopiar');
+	btncopiar.addEventListener("click",function(){
+	document.getElementById('resposta').select();
+	document.execCommand('copy');
+	//resolucao.removeChild('#resposta');
+	document.getElementById('resposta').value = "";
+	//resolucao.removeChild('button');
+	btncopiar.style.display = ('none');
+	textoDigitado.focus();
+	btncriptografar.style.display = ('none');
+})
